cf9b9981d709f32db20563da186f3aef305440e6
ItzcoR <itzcoatlpv@gmail.com>
2019-11-25 15:56:37 -0600

    Modficado script de geracion de aleatorios para el proyecto

---
+++ LetterRandGen.py
@@ -0,0 +1,19 @@
+import random #Generador de archivos con numeros random
+import string
+random.choice(string.ascii_letters)
+random.seed(12)
+n_archivos= int(input("¿Cuantos archivos deseas generar?"))
+randfile = [None]*n_archivos
+for j in range(0 , n_archivos ):
+    randfile[j] = open("RandFiles/RandomArrs" + str(j) + ".txt", "w+" )
+    n=int(input('How many to generate?: '))
+    for i in range(0,n):
+        if(i==n-1):
+            line = random.choice(string.ascii_letters)
+            randfile[j].write(line)
+        else:
+            line = random.choice(string.ascii_letters) + "\n"
+            randfile[j].write(line)
+    #print(line)
+for j in range(0 , n_archivos ):
+    randfile[j].close()
